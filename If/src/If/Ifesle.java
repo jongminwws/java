53e9f3fdfdeab3a6db9ef21241d356a9b7588346
jongminwws <wkdwhd43@naver.com>
2024-04-02 17:38:42 +0900

    java 반복조건문 공부

--- If/src/If/Ifesle.java
+++ If/src/If/Ifesle.java
@@ -16,19 +16,19 @@ public class Ifesle {
             System.out.println("점수는 70미만");
             System.out.println("등급은 D");
         }
-//        int x = 2;
-//        int y = 5;
-//        char c = 'A';
-//        System.out.println(1+x <<33);
-//        System.out.println(y >=5 || x <0 && x >2);
-//        System.out.println(y +=10 - x++);
-//        System.out.println(x+=2);
-//        System.out.println(!('A' <=c && c <='Z'));
-//        System.out.println('C'-c);
-//        System.out.println('5'-'0');
-//        System.out.println(c+1);
-//        System.out.println(++c);
-//        System.out.println(c++);
-//        System.out.println(c);
+        int x = 2;
+        int y = 5;
+        char c = 'A';
+        System.out.println(1+x <<33);
+        System.out.println(y >=5 || x <0 && x >2);
+        System.out.println(y +=10 - x++);
+        System.out.println(x+=2);
+        System.out.println(!('A' <=c && c <='Z'));
+        System.out.println('C'-c);
+        System.out.println('5'-'0');
+        System.out.println(c+1);
+        System.out.println(++c);
+        System.out.println(c++);
+        System.out.println(c);
     }
 }
--- If/src/If/dice.java
+++ If/src/If/dice.java
@@ -1,21 +1,32 @@
 package If;
 
+import java.util.Scanner;
+
 public class dice {
     public static void main(String[] args) {
-        int num = (int) (Math.random()*6)+1;
-
-        if (num == 1){
-            System.out.println("숫자 1");
-        }else if(num == 2){
-            System.out.println("숫자2");
-        } else if (num == 3) {
-            System.out.println("숫자3");
-        } else if (num == 4) {
-            System.out.println("숫자4");
-        } else if (num == 5) {
-            System.out.println("숫자5");
+        char ch = 'A';
+        if (ch == '\t' || ch == ' ') {
+            System.out.println(true);
         }else {
-            System.out.println("숫자6");
+            System.out.println(ch);
         }
+
+        }
+
+//        int num = (int) (Math.random()*6)+1;
+//
+//        if (num == 1){
+//            System.out.println("숫자 1");
+//        }else if(num == 2){
+//            System.out.println("숫자2");
+//        } else if (num == 3) {
+//            System.out.println("숫자3");
+//        } else if (num == 4) {
+//            System.out.println("숫자4");
+//        } else if (num == 5) {
+//            System.out.println("숫자5");
+//        }else {
+//            System.out.println("숫자6");
+//        }
     }
-}
+
