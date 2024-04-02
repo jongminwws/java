82504778b4a0b5a2ed6f992a759a42d704afba59
jongminwws <wkdwhd43@naver.com>
2024-04-02 17:39:15 +0900

    java 변수 공부

--- Ch1/src/ex1/ex1.java
+++ Ch1/src/ex1/ex1.java
@@ -5,6 +5,7 @@ public class ex1 {
     public static void main(String[] args) {
         //콘솔에 출력하는 실행문
         System.out.println("나는 하이");
+
     }//end of main
 } // end of class 이것이 라인 주석이다
 // /* */ 이것은 범위 주석 //
