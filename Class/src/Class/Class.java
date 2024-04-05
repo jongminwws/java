29346a9085e0823eee2c6fc9faf5609fc2ccee75
jongminwws <wkdwhd43@naver.com>
2024-04-05 11:11:39 +0900

    java 클래스 공부

---
+++ Class/src/Class/Class.java
@@ -0,0 +1,8 @@
+package Class;
+
+public class Class {
+    // 라이브러리로서의 코드 (필드, 생성자, 메소드)
+
+
+
+}
---
+++ Class/src/Class/Class1.java
@@ -0,0 +1,11 @@
+package Class;
+
+public class Class1 {
+    public static void main(String[] args) { //실행하기위한 코드
+        Class s1 = new Class();
+        System.out.println("s1 변수가 class 객체를 참조한다");
+
+        Class s2 = new Class();
+        System.out.println("s2 변수가 또 다른 class 객체를 참조한다");
+    }
+}
