b59c6c1fd7a469aaf3414ad6d25a19d35686f1b9
jongminwws <wkdwhd43@naver.com>
2024-04-08 10:59:52 +0900

    java 클래스 공부

--- Class/src/Class/Car2.java
+++ Class/src/Class/Car2.java
@@ -10,17 +10,17 @@ public class Car2 {
     Car2() {
 
     }
-
+    //생성자 2
     Car2(String model) {
         this.model = model;
     }
-
+    //생성자 3
     Car2(String model, String color) {
         this.model = model;
         this.color = color;
 
     }
-
+    //생성자 4
     Car2(String model,String color, int maxSpeed) {
         this.model = model;
         this.color = color;
--- Class/src/Class/Car2exam1.java
+++ Class/src/Class/Car2exam1.java
@@ -3,22 +3,22 @@ package Class;
 public class Car2exam1 {
     public static void main(String[] args) {
 
-
+        //생성자 1 선택
         Car2 car1 = new Car2();
         System.out.println(car1.company);
         System.out.println();
-
+        //생성자 2 선택
         Car2 car2 = new Car2("자가용");
         System.out.println(car2.company);
         System.out.println(car2.model);
         System.out.println();
-
+        //생성자 3 선택
         Car2 car3 = new Car2("자가용","빨강");
         System.out.println(car3.company);
         System.out.println(car3.model);
         System.out.println(car3.color);
         System.out.println();
-
+        //생성자 4 선택
         Car2 car4 = new Car2("자가용", "빨강", 200);
         System.out.println(car4.company);
         System.out.println(car4.color);
