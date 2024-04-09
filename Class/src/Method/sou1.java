4a7f2b7ae454a7b9551dad18d781f3d2e107062e
jongminwws <wkdwhd43@naver.com>
2024-04-09 17:50:15 +0900

    java 클래스(메소드) 공부

--- Class/src/Method/sou1.java
+++ Class/src/Method/sou1.java
@@ -5,7 +5,7 @@ public class sou1 {
         int min = Math.min(a, b);
         int max = Math.max(a, b);
         int sum = 0;
-        for (int i = min ; i <= max; i++) { // a에 낮은숫자가 와야 함 그러면 최소값  최대값이 최소값 보다 크거나 같을 때 까지 i를 1씩 증가
+        for (int i = min ; i <= max; i++) { // a에 낮은숫자가 와야 함 그러면 최소값이 최대값 보다 작거나 같을 때 i는 1씩 증가
             sum += i; // i값을 sum에 저장 
         }
 
