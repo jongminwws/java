2af3a4bc924f3d6ad2e075556428709e74003327
jongminwws <wkdwhd43@naver.com>
2024-04-09 17:47:43 +0900

    java 클래스(메소드) 공부

--- Class/src/Method/sou.java
+++ Class/src/Method/sou.java
@@ -1,10 +1,15 @@
 package Method;
 
+import java.util.Scanner;
+
 public class sou {
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
             sou1 s1 = new sou1();
-            int a = 3;
-            int b = 5;
+            System.out.println("첫번 째 숫자 :");
+            int a = scanner.nextInt();
+            System.out.println("두번 째 숫자 :");
+            int b = scanner.nextInt();
             int result = s1.solution(a, b); // 솔류션 함수 를 불러와서 그 값을 result에 저장을 한다
             System.out.println("결과: " + result); //
         }
